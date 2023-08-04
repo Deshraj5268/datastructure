27aaa68000654dae34098995343509fd6953004b
deshraj <deshrajnitbhopal@gmail.com>
2023-08-04 07:21:15 +0530

    sliding window problems

---
+++ src/queue/slidingwindowvariation/vwindowsize/LongestSubStrWithUniqueChar.java
@@ -0,0 +1,56 @@
+package src.queue.slidingwindowvariation.vwindowsize;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubStrWithUniqueChar {
+
+    public static void main(String[] args) {
+        String input = "aabacbebebe";//aaaa
+        int k = 3;//2 out -1
+        System.out.println("input :"+input+" k:"+k);
+        int result = longestSubStrWithUniqueChar(input,k);
+        System.out.println(result);
+
+    }
+
+    public static int longestSubStrWithUniqueChar(String input,int k){
+        Map<Character,Integer> map = new HashMap<>();
+        Integer mapCharCount;
+        int i=0,j=0;
+        int maxLen=0;
+        char ch;
+        int l=input.length();
+        while(j<l){
+            ch = input.charAt(j);
+            mapCharCount = map.get(ch);
+            if(mapCharCount == null){
+                map.put(ch,1);
+            }else{
+                map.put(ch,mapCharCount+1);
+            }
+            if(map.size()<k){
+                j++;
+            }else if(map.size() == k){
+                if(maxLen < (j-i+1)){
+                    maxLen = j-i+1;
+                }
+                j++;
+            }else{
+                while (map.size() > k){
+                    ch = input.charAt(i);
+                    mapCharCount = map.get(ch);
+                    if(mapCharCount != null && mapCharCount>0){
+                        map.put(ch,mapCharCount-1);
+                    }
+                    if(map.get(ch) == 0){
+                        map.remove(ch);
+                    }
+                    i++;
+                }
+                j++;
+            }
+        }
+        return  (maxLen == 0)?-1:maxLen;
+    }
+}
---
+++ src/queue/slidingwindowvariation/vwindowsize/LongestSubstringWORepeating.java
@@ -0,0 +1,33 @@
+package src.queue.slidingwindowvariation.vwindowsize;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class LongestSubstringWORepeating {
+
+    public static void main(String[] args) {
+
+        String input = "geeksfeeks";
+        int result = longestSubstringWORepeating(input);
+        System.out.println("input : "+input +" result : "+result);
+    }
+
+    public static int longestSubstringWORepeating(String input){
+        Map<Character,Integer> map = new HashMap<>();
+        int i=0,j=0,len = input.length();
+        char nextChar;
+        int maxLen = 0;
+        while(j<len){
+            nextChar = input.charAt(j);
+            if(map.containsKey(nextChar)){
+               i = Math.max(i,map.get(nextChar)+1);
+            }
+            map.put(nextChar,j);
+            maxLen = Math.max(maxLen,(j-i+1));
+            j++;
+        }
+        return maxLen;
+    }
+}
